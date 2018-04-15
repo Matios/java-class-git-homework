cc6ea26cc7ad77e2930c1f605849382a10523712
Matios Dinegdia <h.matios@yahoo.com>
2018-04-15 01:40:20 -0600

    Homw work 16

---
+++ HW16/School.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+  int nextId;
+  List<Student> students;
+
+  public School() {
+    this.nextId = 1;
+    this.students = new ArrayList<Student>();
+  }
+
+  public List<Student> getStudents() {
+    return this.students;
+  }
+
+  public Student getStudentById(int id) {
+    for (int i = 0; i < this.students.size(); i++) {
+      Student currentStudent = this.students.get(i);
+      if (currentStudent.getId() == id) {
+        return currentStudent;
+      }
+    }
+    return null;
+  }
+
+  public void addStudent(Student p) {
+    this.students.add(p);
+  }
+
+  public void removeStudentById(Student p){this.students.remove(p);}
+}
